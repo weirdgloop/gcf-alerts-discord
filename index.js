2eddf988e3dd42b436612132a9dafb35d9398b69
TehKittyCat <tehkittycat@weirdgloop.org>
2022-09-04 01:16:35 -0400

    Customise footer based on resource type.

--- index.js
+++ index.js
@@ -19,11 +19,20 @@ app.listen(port, () => {
 app.post('/', (req, res, next) => {
     if (!req.body.hasOwnProperty('incident')) return // This isn't a suitable Google webhook.
 
+    let footer = req.body.incident.resource_type_display_name
+    if ( req.body.incident.resource.type == 'cloudsql_database' ) {
+        footer += ' • ' + req.body.incident.resource_display_name
+    } else if ( req.body.incident.resource.type == 'k8s_container' ) {
+        footer += ' • ' + req.body.incident.resource.labels.cluster_name + '/' + req.body.incident.resource.labels.container_name
+    } else if ( req.body.incident.hasOwnProperty('metric') && req.body.incident.metric.type == 'logging.googleapis.com/user/Redis-Fatal-Errors' ) {
+        footer += ' • ' + req.body.incident.metric.labels.server
+    }
+
     let embed = new discord.MessageEmbed()
         .setTitle(req.body.incident.policy_name)
         .setColor('#f58a42')
         .setDescription(req.body.incident.summary)
-        .setFooter(req.body.incident.resource_type_display_name)
+        .setFooter(footer)
         .setURL(req.body.incident.url)
         .setTimestamp(new Date(req.body.incident.started_at * 1000));
     
