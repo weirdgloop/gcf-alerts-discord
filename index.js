e863641bdbf8645bed18e112b268e9edc71302b0
TehKittyCat <tehkittycat@weirdgloop.org>
2023-07-26 22:16:38 -0400

    Tidy up incident summary into fields.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv');
-const express = require('express');
+require('dotenv')
+const express = require('express')
 const discord = require('discord.js')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 
-const app = express();
-const port = 6969;
+const app = express()
+const port = 6969
 
 const webhookClient = new discord.WebhookClient({
     url: process.env.DISCORD_WEBHOOK_URL
@@ -13,38 +13,51 @@ const webhookClient = new discord.WebhookClient({
 app.use(bodyParser.json())
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+    console.log(`Server running on port ${port}`)
+})
 
 app.post('/', (req, res, next) => {
     if (!req.body.hasOwnProperty('incident')) return // This isn't a suitable Google webhook.
-
-    let footer = req.body.incident.resource_type_display_name
-    if ( req.body.incident.resource.type == 'cloudsql_database' ) {
-        footer += ' • ' + req.body.incident.resource_display_name
-    } else if ( req.body.incident.resource.type == 'k8s_container' ) {
-        footer += ' • ' + req.body.incident.resource.labels.cluster_name + '/' + req.body.incident.resource.labels.container_name
-    } else if ( req.body.incident.hasOwnProperty('metric') && req.body.incident.metric.type == 'logging.googleapis.com/user/Redis-Fatal-Errors' ) {
-        footer += ' • ' + req.body.incident.metric.labels.server
-    }
+    console.dir(req.body, { depth: null })
 
     let embed = new discord.MessageEmbed()
-        .setTitle(req.body.incident.policy_name)
-        .setColor('#f58a42')
-        .setDescription(req.body.incident.summary)
-        .setFooter(footer)
-        .setURL(req.body.incident.url)
-        .setTimestamp(new Date(req.body.incident.started_at * 1000));
+            .setTitle(req.body.incident.policy_name)
+            .setColor('#F58A42')
+            .setURL(req.body.incident.url)
+            .setTimestamp(new Date(req.body.incident.started_at * 1000))
+
+    let resource = req.body.incident.resource_type_display_name
+    if ( req.body.incident.resource.type == 'cloudsql_database' ) {
+        resource += ' • ' + req.body.incident.resource_display_name
+    } else if ( req.body.incident.resource.type == 'k8s_container' ) {
+        resource += ' • ' + req.body.incident.resource.labels.cluster_name + '/' + req.body.incident.resource.labels.container_name
+    }
+
+    if ( req.body.incident.hasOwnProperty('metric') ) {
+        if ( req.body.incident.metric.labels.hasOwnProperty('environment') ) {
+            embed.addField('Environment', req.body.incident.metric.labels.environment)
+        } else {
+            embed.addField('Resource', resource)
+        }
+        embed.addFields([
+            { name: 'Error', value: req.body.incident.condition_name },
+            { name: 'Threshold', value: req.body.incident.threshold_value },
+            { name: 'Value', value: req.body.incident.observed_value },
+        ])
+    } else {
+        embed.setDescription(req.body.incident.summary)
+             .setFooter(resource)
+    }
     
     webhookClient.send({
         username: 'Google Cloud',
         embeds: [embed]
-    });
+    })
 
-    console.log('Received incident. Sending to Discord.');
-    res.send(req.body);
-});
+    console.log('Received incident. Sending to Discord.')
+    res.send(req.body)
+})
 
 module.exports = {
     app
-};
+}
